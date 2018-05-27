0de216b07c592923cf765f8e95c5d2b507017456
Christofer Chan <devchumbing@gmail.com>
2018-05-27 03:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -135,15 +135,13 @@ function moveDodgerRight() {
  */
 
 
-function positionToInteger(p) {
-  return parseInt(p.split('px')[0]) || 0
-}
-
 function start() {
-  document.addEventListener('keydown', moveDodger)
+  window.addEventListener('keydown', moveDodger)
 
   START.style.display = 'none'
 
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 100
+  }, 1000)
+}
+
